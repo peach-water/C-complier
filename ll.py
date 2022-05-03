f8e76c89df201a8465fc7930ab50e78cfaa13e02
peach-water <wang.peach@qq.com>
2022-05-03 16:06:07 +0800

    四元式完整版

--- ll.py
+++ ll.py
@@ -1,10 +1,32 @@
+id = 0
+have_letter = False
 
+
+def identify(string):
+    if string[0] >= '0' and string[0] <= '9':
+        return True
+    if string[0] >= 'a' and string[0] <= 'z':
+        return True
+    return False
+
+
+def compute(a, b, symbol):
+    if symbol == '+':
+        return a+b
+    elif symbol == '-':
+        return a-b
+    elif symbol == '*':
+        return a*b
+    elif symbol == '/':
+        return a/b
+    raise "symbol error not in +-*/"
 
 # states = ['E','E1','T','T1','F']
 
+
 def nextState(state, input):
     if state == 'E':
-        if input == 'identify':
+        if identify(input):
             return ['T', 'E1']
         elif input == '(':
             return ['T', 'E1']
@@ -16,7 +38,7 @@ def nextState(state, input):
         elif input == ')' or input == '#':
             return ['#']
     elif state == 'T':
-        if input == 'identify' or input == '(':
+        if identify(input) or input == '(':
             return ['F', 'T1']
     elif state == 'T1':
         if input == '+' or input == '-' or input == ')' or input == '#':
@@ -26,7 +48,7 @@ def nextState(state, input):
                 return ['*', 'F', 'T1']
             return ['/', 'F', 'T1']
     elif state == 'F':
-        if input == 'identify':
+        if identify(input):
             return ['identify']
         elif input == '(':
             return ['(', 'E', ')']
@@ -35,34 +57,57 @@ def nextState(state, input):
     raise "error"
 
 
-def identify(string):
-    if string[0] >= '0' and string[0] <= '9':
-        return True
-    if string[0] >= 'a' and string[0] <= 'z':
-        return True
-    return False
-
-
-
+def generate(equal):
+    global id
+    global have_letter
+    if not have_letter:
+        try:
+            a = float(equal[0])
+            b = float(equal[2])
+            ret = [equal[1], a, b, compute(a, b, equal[1])]
+        except:
+            id += 1
+            have_letter = True
+            ret = [equal[1], equal[0], equal[2], "T"+str(id)]
+    else:
+        id += 1
+        ret = [equal[1], equal[0], equal[2], "T"+str(id)]
+    return ret
 
 
 def LL(equal):
-    print("%20s%10s"%("规约式","栈顶"))
-    for i in range(len(equal)):
-        if identify(equal[i]):
-            equal[i] = 'identify'
+    # print("%20s%10s" % ("规约式", "栈顶"))
+    print("四元式计算过程")
+
     index = 0
     stack = ['#', 'E']
+    sematic_stack = []
+
     while index < len(equal):
         try:
             temp = nextState(stack[-1], equal[index])
-            print("%2s -> %18s" % (stack[-1], str(temp)), end='  ')
-            print(stack)
-            if temp[0] == equal[index]:
+            # print("%2s -> %18s" % (stack[-1], str(temp)), end='  ')
+            # print(stack)
+            if temp[0] == 'identify' and identify(equal[index]) or equal[index] == temp[0]:
+                if identify(equal[index]):
+                    sematic_stack.append(equal[index])
+                    if len(sematic_stack) > 2 and sematic_stack[-2] != '(':
+                        if sematic_stack[-2] in "*/" or (equal[index+1] not in "*/"):
+                            q = generate(sematic_stack[-3:])
+                            sematic_stack = sematic_stack[:-3]
+                            sematic_stack.append(q[3])
+                            print(q)
+                else:
+                    sematic_stack.append(equal[index])
                 index += 1
                 temp.pop(0)
             elif equal[index] == ')' and stack[-1] == ')':
+                if sematic_stack[-2] == '(':
+                    t = sematic_stack[-1]
+                    sematic_stack = sematic_stack[:-2]
+                    sematic_stack.append(t)
                 index += 1
+
             stack.pop(-1)
             for i in range(len(temp)-1, -1, -1):
                 if temp[i] != '#':
@@ -75,8 +120,8 @@ def LL(equal):
         # 规约
         try:
             temp = nextState(stack[-1], '#')
-            print("%2s -> %18s"%(stack[-1], str(temp)), end='  ')
-            print(stack)
+            # print("%2s -> %18s" % (stack[-1], str(temp)), end='  ')
+            # print(stack)
             stack.pop(-1)
             for i in range(len(temp)-1, -1, -1):
                 if temp[i] != '#':
@@ -88,6 +133,12 @@ def LL(equal):
         # 最后检查规约失败
         print("error")
         return
+    else:
+        while len(sematic_stack) > 1:
+            q = generate(sematic_stack[-3:])
+            sematic_stack = sematic_stack[:-3]
+            print(q)
+            sematic_stack.append(q[3])
     print("accept !!!")
 
 
@@ -96,14 +147,15 @@ if __name__ == "__main__":
         equal = fp.readlines()
     fp.close()
 
-    # print("分析 %s"%(equal[0]))
+    # print("分析 %s" % (equal[0]))
     # temp = equal[0].split()
     # LL(equal=temp)
     # print()
 
     for i in range(len(equal)):
-        print("分析 %s"%(equal[i]))
+        id = 0
+        have_letter = False
+        print("分析 %s" % (equal[i]))
         temp = equal[i].split()
         LL(equal=temp)
         print()
-
